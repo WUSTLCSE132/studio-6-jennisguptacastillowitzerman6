ba8853446ed96c1422e93283ebe8d4921f44e918
mattw <mattw@DESKTOP-VKBA7OP.dhcp.wustl.edu>
2017-10-09 12:55:42 -0500

    Assignment 6

---
+++ debounce/debounce.ino
@@ -0,0 +1,31 @@
+volatile int count = 0;
+const int buttonPin = 2;
+int buttonState;
+int lastButtonState = LOW;
+
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50; 
+
+void buttonPressed() {
+ 
+  if((millis() - lastDebounceTime) > debounceDelay){
+    count+= 1;
+    Serial.println(count);
+    lastDebounceTime = millis();
+  }
+
+  
+}
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  // Interrupts can happen on "edges" of signals.  
+  // Three edge types are supported: CHANGE, RISING, and FALLING 
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonPressed, RISING);
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+}
--- src/studio6/SerialComm.java
+++ src/studio6/SerialComm.java
@@ -2,10 +2,22 @@ package studio6;
 
 import jssc.*;
 
+
 public class SerialComm {
+
 
 	SerialPort port;
 
+	public static void main(String args[]) throws SerialPortException{
+		SerialComm port = new SerialComm("COM4");
+		while(true) {
+			if(port.available()) {
+				char read = (char) port.readByte();
+				System.out.println(read);
+			}
+		}
+	}
+	
 	private boolean debug;  // Indicator of "debugging mode"
 	
 	// This function can be called to enable or disable "debugging mode"
@@ -13,6 +25,7 @@ public class SerialComm {
 		debug = mode;
 	}	
 	
+
 
 	// Constructor for the SerialComm class
 	public SerialComm(String name) throws SerialPortException {
@@ -26,11 +39,29 @@ public class SerialComm {
 		debug = false; // Default is to NOT be in debug mode
 	}
 		
-	// TODO: Add writeByte() method from Studio 5
+	public void writeByte(byte Byte) {
+		if(debug) {
+			try {
+				port.writeByte(Byte);
+				System.out.println("0x"+ Integer.toHexString(Byte));
+			} 
+			catch (SerialPortException e) {
+				e.printStackTrace();
+			}	
+		}
+	}
 	
-	// TODO: Add available() method
+	public boolean available() throws SerialPortException{
+		if((port.getInputBufferBytesCount()) > 0)
+			return true;
+		else
+			return false;
+	}
 	
-	// TODO: Add readByte() method	
-	
-	// TODO: Add a main() method
+	public byte readByte() throws SerialPortException{
+		byte[] input = port.readBytes(1);
+		return input[0];
+	}
 }
+
+
